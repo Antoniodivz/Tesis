07064cd2fac89f07a61cd9270316a3278bca906a
Marco Antonio Diaz Villarreal <toodivz@gmail.com>
2021-02-11 17:17:25 -0600

    correcciones

--- Hoti_fractal/scritp_run.sh
+++ Hoti_fractal/scritp_run.sh
@@ -2,6 +2,9 @@
 
 for delta in 0 10 20 30 40 50 60;
 do
-	cd /home/toodivz/Documents/Tesis/Hoti_curadrado/Delta_$delta
-	papermill Hoti_$delta.ipynb Hoti_$delta.output.ipynb
+	cd /home/toodivz/Documents/Tesis/Hoti_fractal/Delta_$delta
+	papermill Hoti_fractal_$delta.ipynb Hoti_fractal_$delta.output.ipynb
 done
+
+cd /home/toodivz/Documents/Tesis/Hoti_fractal/54x54
+papermill Hoti_fractal_54.ipynb Hoti_fractal_54.output.ipynb
