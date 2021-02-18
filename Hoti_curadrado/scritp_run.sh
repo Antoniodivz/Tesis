6dc83f921c58d9c5c573dc1952a208f8da578ac5
Marco Antonio Diaz Villarreal <toodivz@gmail.com>
2021-02-18 03:13:52 -0600

    calculos realizados

--- Hoti_curadrado/scritp_run.sh
+++ Hoti_curadrado/scritp_run.sh
@@ -3,12 +3,12 @@
 cd /home/toodivz/Documents/Tesis/Hoti_curadrado/Delta_0
         papermill Hoti_0.ipynb Hoti_0.output.ipynb -p name_array array_18x18.xyz -p epsi 0.0 \
 		                 -p cal_value True
-value=$(cat /home/toodivz/Documents/Tesis/Hoti_curadrado/Delta_0/gamma_value)
+value=$(cat /home/toodivz/Documents/Tesis/Hoti_curadrado/Delta_0/gamma_value.txt)
 
 
 for delta in 0.01 0.05 0.1 0.2 0.3 0.4 0.5;
 do
 	cd /home/toodivz/Documents/Tesis/Hoti_curadrado/Delta_$delta
-	papermill Hoti_$delta.ipynb Hoti_$delta.output.ipynb -p name_array array_18x18.xyz -p epsi $delta \
-		-p nval value -p cal_value False
+	papermill Hoti_0.ipynb Hoti_$delta.output.ipynb -p name_array array_18x18.xyz -p epsi $delta \
+		-p nval $value -p cal_value False
 done
--- Hoti_fractal/scritp_run.sh
+++ Hoti_fractal/scritp_run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-for delta in 0 10 20 30 40 50 60;
+cd /home/toodivz/Documents/Tesis/Hoti_fractal/Delta_0
+        papermill Hoti_fractal.ipynb Hoti_fractal.output.ipynb -p name_array LatticeSierpinski_18x18.xyz -p epsi 0.0 \
+		                 -p cal_value True
+value=$(cat /home/toodivz/Documents/Tesis/Hoti_fractal/Delta_0/gamma_value.txt)
+
+
+for delta in 0.01 0.05 0.1 0.2 0.3 0.4 0.5;
 do
 	cd /home/toodivz/Documents/Tesis/Hoti_fractal/Delta_$delta
-	papermill Hoti_fractal_$delta.ipynb Hoti_fractal_$delta.output.ipynb
+	papermill Hoti_fractal.ipynb Hoti_fractal_$delta.output.ipynb -p name_array LatticeSierpinski_18x18.xyz -p epsi $delta \
+		-p nval $value -p cal_value False
 done
-
-#cd /home/toodivz/Documents/Tesis/Hoti_fractal/54x54
-#papermill Hoti_fractal_54.ipynb Hoti_fractal_54.output.ipynb
